8d93fb77820b1534982ec75bc5fc8e5ec38a0df0
andreapanico12 <andreapanico12@gmail.com>
2025-01-28 15:05:20 +0100

    feat: add getFlag function tu ResultCard component to manage the flag image dinamically

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,11 +1,26 @@
 
 
 function ResultCard({title, originalTitle,language,vote}) {
+
+  const getFlag = (languageIn) =>{
+    const countryIn = languageIn === "en" ? "gb": languageIn;
+    return `https://flagcdn.com/w40/${countryIn}.png`
+  }
+
+
   return (
     <div className="card">
       <h3>{title}</h3>
       <p><strong>Titolo Originale:</strong>{originalTitle}</p>
-      <p><strong>Lingua:</strong>{language}</p>
+      <p>
+        <strong>Lingua:</strong>{language}
+        <img 
+        src={getFlag(language)} 
+        alt={language}
+        onError={(e) => (e.target.style.display= "none")}
+        style={{width: "40px" , marginLeft: "5px"}} 
+        />
+      </p>
       <p><strong>Voto:</strong>{vote}</p>
     </div>
   )
